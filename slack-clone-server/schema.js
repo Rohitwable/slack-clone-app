62399341ef62dd4b7757fb8dbe6f61a39e9dda87
Rohitwable <rohitwable46@gmail.com>
2020-10-31 19:29:17 +0530

    server: graphql scalar types for user message channel team

--- slack-clone-server/schema.js
+++ slack-clone-server/schema.js
@@ -1,4 +1,30 @@
 export default `
+type Team {
+    owner: User!
+    members: [User!]!
+    channels: [Channels!]!
+}
+
+type Channel {
+    id: Int!
+    name: String!
+    public: Boolean!
+    messages: [Message!]!
+    users: [User!]!
+}
+
+type Message {
+    id: Int!
+    text: String!
+    user: User!
+    channel: Channel!
+}
+type User {
+    id: Int!
+    username: String
+    email: String!
+    teams: [Team!]!
+}
     type Query {
         hi: String!
     }
